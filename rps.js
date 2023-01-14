e915a66809fd800ec1cad14e262900dee8cbdadf
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-01-13 17:16:44 -0800

    Add plan and pseudocode for functions

--- rps.js
+++ rps.js
@@ -1,5 +1,34 @@
 /*
 
-- Create a program that plays a single round of Rock, Paper, Scissors against a computer.
+Create a program that plays Rock, Paper, Scissors against a computer.
+
+Initial plan:
+- Play one round
+    - Prompt user for their choice for Rock, Paper, Scissors.
+    - Pick a random choice for the computer.
+    - Compare the choices to determine a winner.
+    - Inform user of the results.
 
 */
+
+//first, we prompt the user.
+let playerMove = prompt("Let's play Rock, Paper, Scissors! Choose your move: ").toLowerCase();
+
+function getComputerChoice() {
+    //Pick randomly from Rock, paper, scissors.
+    //return the choice.
+}
+
+function playRound(playerSelection, computerSelection) {
+    //if values are equal, then it is a tie.
+    //if rock
+        //loses to paper
+        //wins against scissors
+    //if scissors
+        //loses to rock
+        //wins against paper
+    //if paper
+        //loses to scissors
+        //wins against rock
+    //return if player won or lost against computer.
+}
