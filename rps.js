31362c71ecf0c1d6abec43323265e3e334a5cf82
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-01-13 18:07:12 -0800

    Add multi-round functionality

--- rps.js
+++ rps.js
@@ -11,12 +11,51 @@ Initial plan:
 
 */
 
-//first, we prompt the user.
 const moves = ["rock", "paper", "scissors"];
-let playerMove = getPlayerChoice();
-let computerMove = getComputerChoice();
-playRound(playerMove, computerMove);
 
+let playerScore = 0;
+let computerScore = 0;
+let draw = 0;
+let round = 1;
+let playerMove, computerMove, winner;
+
+//game loop, 5 rounds
+while (round <= 5) {
+    if (round > 1) {
+        console.log("Let's play another round!");
+    }
+    playerMove = getPlayerChoice();
+    computerMove = getComputerChoice();
+    winner = playRound(playerMove, computerMove);
+    switch(winner) {
+        case 0:
+            computerScore++;
+            break;
+        case 1:
+            playerScore++;
+            break;
+        case 2:
+            draw++;
+            break;
+        default:
+            console.log("Error, something went wrong.")
+    }
+    console.log("Player: " + playerScore + " Computer: " + computerScore + " Draws: " + draw);
+    round++;
+}
+
+//Game end prompt
+console.log("Game over!")
+
+if (playerScore > computerScore) {
+    console.log("You won!!");
+} else if (playerScore === computerScore) {
+    console.log("It's a tie!!");
+} else {
+    console.log("Computer wins!!");
+}
+
+//Checks if player picked a valid move
 function getPlayerChoice() {
     let move = prompt("Let's play Rock, Paper, Scissors! Choose your move.").toLowerCase();
     while (!moveCheck(move)) {
@@ -26,13 +65,13 @@ function getPlayerChoice() {
     return move;
 }
 
+//helper function for getPlayerChoice
 function moveCheck(move) {
     return move === "rock" || move === "paper" || move === "scissors";
 }
 
+//returns computer's move
 function getComputerChoice() {
-    //Pick randomly from Rock, paper, scissors.
-    //return the choice.
     let move = moves[Math.floor(Math.random() * (moves.length - 1))];
     console.log("Computer chose " + move.toUpperCase());
     return move;
@@ -52,24 +91,31 @@ function playRound(playerSelection, computerSelection) {
     //return if player won or lost against computer.
     if (playerSelection === computerSelection) {
         console.log("It's a tie!");
+        return 2;
     } else {
         if (playerSelection === "rock") {
             if (computerSelection === "scissors") {
                 console.log("You win! Rock beats scissors!");
+                return 1;
             } else if (computerSelection === "paper") {
                 console.log("You lose! Paper beats rock!");
+                return 0;
             }
         } else if (playerSelection === "scissors") {
             if (computerSelection === "paper") {
                 console.log("You win! Scissors beats paper!");
+                return 1;
             } else if (computerSelection === "rock") {
                 console.log("You lose! Rock beats scissors!");
+                return 0;
             }
         } else if (playerSelection === "paper") {
             if (computerSelection === "rock") {
                 console.log("You win! Paper beats rock!");
+                return 1;
             } else if (computerSelection === "scissors") {
                 console.log("You lose! Scissors beats paper!");
+                return 0;
             }
         }
     }
